bc78d9a5788a46d7ad26e14ae521f6b72ccbf822
nick <nckgriva208@gmail.com>
2021-07-27 14:07:57 +0700

    remove HW

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import 'https://unpkg.com/purecss@2.0.6/build/pure-min.css';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
